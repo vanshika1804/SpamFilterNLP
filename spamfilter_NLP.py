68dc385c13f90373bf8379cb0515c0ea78a4b5a1
Vanshika Bhatia <vanshika18042000@gmail.com>
2021-03-07 14:28:34 +0530

    Data pre-processing

--- spamfilter_NLP.py
+++ spamfilter_NLP.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def importData(path):
     data = [
         line.rstrip() for line in open(path)
@@ -5,16 +8,23 @@ def importData(path):
     print(f'{len(data)} data lines imported!')
     return data
 
+
 def printData(data):
     for message_index, message in enumerate(data[:10]):
         print(f'{message_index} --> {message}')
 
-def main():
-    data = importData('./SMSSpamCollection')
-    printData(data)
 
+def convertDataToDataFrame(path):
+    df = pd.read_csv(path, sep='\t', names=['label', 'msg'] )
+    return df
+
+
+def main():
+    path = './SMSSpamCollection'
+    # data = importData(path)
+    # printData(data)
+    df = convertDataToDataFrame(path)
 
 
 if __name__ == '__main__':
     main()
-    
